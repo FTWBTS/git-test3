034bde8d8c7673a607326de74c97aba7e5ce83de
guhlong <2530266758@qq.com>
2022-08-22 12:04:45 +0800

    这是我的尝试2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!GIThahahah");
+        System.out.println("Hello world!GIThahahah2222");
     }
 }
